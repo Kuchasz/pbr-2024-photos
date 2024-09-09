f51241daaef3285979e593418024da8720ffa692
Kuchasz <kuchasz1@o2.pl>
2024-09-09 21:09:08 +0200

    newer title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Poland Business Run 2024",
+  title: "Poland Business Run 2024 Photos Browser",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
